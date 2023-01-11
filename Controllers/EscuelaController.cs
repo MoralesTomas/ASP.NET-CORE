c78f327ef49b5d1db00531344f7fd234988bb4fe
AlejoMora991014 <alejandroester1910@gmail.com>
2023-01-11 13:36:54 -0600

    Definiendo un modelo a nivel basico

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -1,3 +1,4 @@
+using ASP.NET_CORE.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_CORE.Controllers
@@ -6,7 +7,13 @@ namespace ASP.NET_CORE.Controllers
     {
         public IActionResult Index(){
             
-            return View();
+            var escuela = new Escuela();
+            escuela.Nombre = "U.A";
+            escuela.AñoFundacion = 2005;
+            escuela.EscuelaId = Guid.NewGuid().ToString();
+            
+            //mandamos la escuela como modelo hacia la vista.
+            return View( escuela );
         }
     }
 }
---
+++ Models/Escuela.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_CORE.Models
+{
+    public class Escuela
+    {
+        public string EscuelaId { get; set; }
+        public string Nombre { get; set; }
+        public int AñoFundacion { get; set; }
+    }
+}
